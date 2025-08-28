cab851d6caed2d28cb69c6fc65bb1a31cedac146
XingQuan <xingquan@mysoftinn.com>
2025-08-28 15:55:15 +0800

    create dtos

---
+++ PreliminaryTechnicalQuestion/Domain/BookingDto.cs
@@ -0,0 +1,11 @@
+﻿namespace PreliminaryTechnicalQuestion.Domain
+{
+    public class BookingDto
+    {
+        public int Id { get; set; }
+        public string GuestName { get; set; }
+        public int RoomId { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+    }
+}
---
+++ PreliminaryTechnicalQuestion/Domain/RoomDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PreliminaryTechnicalQuestion.Domain
+{
+    public class RoomDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}

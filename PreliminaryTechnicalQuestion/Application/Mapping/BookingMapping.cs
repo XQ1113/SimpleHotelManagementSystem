0b2da6a28c9358e9b5af8ecb5b4da96a6a2dd2d2
XingQuan <xingquan@mysoftinn.com>
2025-08-30 15:45:07 +0800

    mapping

---
+++ PreliminaryTechnicalQuestion/Application/Mapping/BookingMapping.cs
@@ -0,0 +1,38 @@
+﻿using PreliminaryTechnicalQuestion.Application.Dto;
+
+namespace PreliminaryTechnicalQuestion.Application.Mapping
+{
+    public static class BookingMapping
+    {
+        public static BookingDto TBookingDto(this Domain.Entities.Booking booking)
+        {
+            return new(
+                 booking.Id,
+                 booking.GuestName,
+                 booking.RoomId,
+                 booking.CheckInDate,
+                 booking.CheckOutDate,
+                 booking.Room.Name,
+                 booking.Room.Type);
+        }
+
+        public static BookingDto TBookingDto(this Domain.Entities.Booking booking, Domain.Entities.Room room)
+        {
+            return new(
+                 booking.Id,
+                 booking.GuestName,
+                 booking.CheckInDate,
+                 booking.CheckOutDate,
+                 room);
+        }
+
+        public static Domain.Entities.Booking TBookingEntity(this CreateBookingDto booking)
+        {
+            return new(
+                booking.GuestName,
+                booking.RoomId,
+                booking.CheckInDate,
+                booking.CheckOutDate);
+        }
+    }
+}
---
+++ PreliminaryTechnicalQuestion/Application/Mapping/RoomMapping.cs
@@ -0,0 +1,33 @@
+﻿using PreliminaryTechnicalQuestion.Application.Dto;
+
+namespace PreliminaryTechnicalQuestion.Application.Mapping
+{
+    public static class RoomMapping
+    {
+        public static RoomDto TRoomDto(this PreliminaryTechnicalQuestion.Domain.Entities.Room room)
+        {
+            return new(
+                room.Id,
+                room.Name,
+                room.Type,
+                room.IsAvailable);
+        }
+
+        public static Domain.Entities.Room TRoomEntity(this CreateRoomDto room)
+        {
+            return new(
+                room.Name,
+                room.Type,
+                room.IsAvailable);
+        }
+
+        public static Domain.Entities.Room TRoomEntity(this UpdateRoomDto room, int id)
+        {
+            return new(
+                id,
+                room.Name,
+                room.Type,
+                room.IsAvailable);
+        }
+    }
+}

f42dd8a43cc2d5ef81e4a296b641efdc860551cd
XingQuan <xingquan@mysoftinn.com>
2025-08-30 15:45:50 +0800

    endpoint

---
+++ PreliminaryTechnicalQuestion/Presentation/BookingEndpoint.cs
@@ -0,0 +1,28 @@
+﻿using PreliminaryTechnicalQuestion.Application.Booking.Command;
+using PreliminaryTechnicalQuestion.Application.Booking.Query;
+
+namespace PreliminaryTechnicalQuestion.Presentation
+{
+    public static class BookingEndpoint
+    {
+        public static RouteGroupBuilder MapBookingEndpoint(this WebApplication app)
+        {
+            var group = app.MapGroup("bookings");
+
+            // get all bookings
+            group.MapGet("/", GetBookings.GetBookingsAsync);
+
+            // get specific booking
+            group.MapGet("/{id}", GetBooking.GetBookingAsync)
+            .WithName("GetBooking");
+
+            // add booking
+            group.MapPost("/", AddBooking.AddBookingAsync);
+
+            // delete specific booking
+            group.MapDelete("/{id}", DeleteBooking.DeleteBookingAsync);
+
+            return group;
+        }
+    }
+}
---
+++ PreliminaryTechnicalQuestion/Presentation/RoomEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using PreliminaryTechnicalQuestion.Application.Room.Command;
+using PreliminaryTechnicalQuestion.Application.Room.Query;
+
+namespace PreliminaryTechnicalQuestion.Presentation
+{
+    public static class RoomEndpoint
+    {
+        public static RouteGroupBuilder MapRoomEndpoint(this WebApplication app)
+        {
+            var group = app.MapGroup("rooms");
+
+            // get all rooms
+            group.MapGet("/", GetRooms.GetRoomsAsync);
+
+            // get all available rooms
+            group.MapGet("/available", GetAvailableRooms.GetAvailableRoomsAsync);
+
+            // get specific room
+            group.MapGet("/{id}", GetRoom.GetRoomAsync)
+            .WithName("GetRoom");
+
+            // add rooms
+            group.MapPost("/", AddRooms.AddRoomsAsync);
+
+            // update specific room
+            group.MapPut("/{id}", UpdateRoom.UpdateRoomAsync);
+
+            return group;
+        }
+    }
+}
